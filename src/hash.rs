55e10d3e03eac49a9708336aa2b27bce025fe672
Ciekce <kantaja98@gmail.com>
2023-12-01 20:34:07 +0000

    true random seed!!!

--- src/hash.rs
+++ src/hash.rs
@@ -27,7 +27,7 @@ const TOTAL_SIZE: usize = COLOR_SQUARE_SIZE + STM_SIZE;
 const COLOR_SQUARE_OFFSET: usize = 0;
 const STM_OFFSET: usize = COLOR_SQUARE_OFFSET + COLOR_SQUARE_SIZE;
 
-const HASHES: [u64; TOTAL_SIZE] = rng::fill_u64_array(1234);
+const HASHES: [u64; TOTAL_SIZE] = rng::fill_u64_array(0x22ff7d8af027681b);
 
 #[must_use]
 pub fn color_square_key(c: Color, sq: Square) -> u64 {
